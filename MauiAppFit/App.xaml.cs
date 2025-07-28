f92d6015fe1d1712a3b394d03e3f8e95a12e1c55
231008 <231008@CPD01-M19>
2025-07-28 15:56:51 -0300

    AppShell and SQLiteDataBaseHelper

--- MauiAppFit/App.xaml.cs
+++ MauiAppFit/App.xaml.cs
@@ -1,10 +1,30 @@
-﻿namespace MauiAppFit
+﻿using MauiAppFit.Helpers;
+
+namespace MauiAppFit
 {
     public partial class App : Application
     {
+        static SQLiteDataBaseHelper database;
+
         public App()
         {
             InitializeComponent();
+        }
+
+        public static SQLiteDataBaseHelper Database
+        {
+            get
+            {
+                if(database == null)
+                {
+                    database = new SQLiteDataBaseHelper(
+                        Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "XamAppFit.db3"));
+                }
+
+                return database;
+            }
+
+            
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
