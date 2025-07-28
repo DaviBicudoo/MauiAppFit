7f20b121d470f324dd7abb2b39e8b6c20be6f0d9
231008 <231008@CPD01-M19>
2025-07-28 10:52:45 -0300

    SQLiteDatabaseHelper class

--- MauiAppFit/Helpers/SQLiteDataBaseHelper.cs
+++ MauiAppFit/Helpers/SQLiteDataBaseHelper.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
+using MauiAppFit.Models;
 
 namespace MauiAppFit.Helpers
 {
-    internal class SQLiteDataBaseHelper
+    public class SQLiteDataBaseHelper
     {
+        readonly SQLiteAsyncConnection _dbConnection;
+
+        public SQLiteDataBaseHelper(string dbPath)
+        {
+            _dbConnection = new SQLiteAsyncConnection(dbPath);
+            _dbConnection.CreateTableAsync<Activity>().Wait();
+        }
     }
 }
