3376a6b6215b957561a0889592b8aef07c73ca5c
231008 <231008@CPD01-M19>
2025-07-28 10:40:04 -0300

    Activity model

--- MauiAppFit/Models/Activity.cs
+++ MauiAppFit/Models/Activity.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
 
 namespace MauiAppFit.Models
 {
     public class Activity
     {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string? Description { get; set; }
+        public DateTime Date { get; set; }
+        public double? Weight { get; set; }
+        public string? Observations { get; set; }
     }
 }
