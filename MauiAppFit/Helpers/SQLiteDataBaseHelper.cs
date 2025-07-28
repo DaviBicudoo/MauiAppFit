4d9fafb60ca530124663c4bce6674aff2b305e61
231008 <231008@CPD01-M19>
2025-07-28 15:10:30 -0300

    SQLiteDataBaseHelper finished

--- MauiAppFit/Helpers/SQLiteDataBaseHelper.cs
+++ MauiAppFit/Helpers/SQLiteDataBaseHelper.cs
@@ -17,5 +17,45 @@ namespace MauiAppFit.Helpers
             _dbConnection = new SQLiteAsyncConnection(dbPath);
             _dbConnection.CreateTableAsync<Activity>().Wait();
         }
+
+        public Task<List<Activity>> GetAllRows()
+        {
+            return _dbConnection.Table<Activity>().OrderByDescending(i => i.ID).ToListAsync();
+        }
+
+        public Task<Activity> GetById(int id)
+        {
+            return _dbConnection.Table<Activity>().FirstAsync(i => i.ID == id);
+        }
+
+        public Task<int> Insert(Activity model)
+        {
+            return _dbConnection.InsertAsync(model); 
+        }
+
+        public Task<List<Activity>> Update(Activity model)
+        {
+            string SQL = "UPDATE Activity SET Description=?, Date=?, Weight=?, Observations=? WHERE ID=?";
+
+            return _dbConnection.QueryAsync<Activity>(
+                SQL,
+                model.Description,
+                model.Date,
+                model.Weight,
+                model.Observations,
+                model.ID);
+        }
+
+        public Task<int> Delete(int ID)
+        {
+            return _dbConnection.Table<Activity>().DeleteAsync(i => i.ID == ID);
+        }
+
+         public Task<List<Activity>> Search(string query)
+         {
+            string SQL = $"SELECT * FROM Activity WHERE Description LIKE '%{query}'";
+
+            return _dbConnection.QueryAsync<Activity>(query);
+         }
     }
 }
